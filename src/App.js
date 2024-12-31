1399a5d77cf5a207bec45c63b84adc902eb76db6
ranjeetkirat <ranjeetkirat000@gmail.com>
2024-12-31 16:56:02 +0530

    new update remove router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sider from './component/sider';
-import About from './component/About';
+
 
 
 function App() {
   return (
     <div className="App">
- <BrowserRouter>
- <Sider/>
- 
-  <Routes>
-
-    <Route path='/about' element={<About/>}/>
-  </Routes>
- </BrowserRouter>
+<Sider/>
     </div>
   );
 }
