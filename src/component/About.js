41c011b304d7e4bca6d33c78f4654433ebd34f02
ranjeetkirat <ranjeetkirat000@gmail.com>
2024-12-31 13:16:39 +0530

    new about change

--- src/component/About.js
+++ src/component/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <>
         <div >
-            <h1 className="text-3xl font-semibold mt-10">About Todo App</h1>
+            <h1 className="text-3xl font-semibold mt-10"> Todo App</h1>
 
            <p className="mt-5 "> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima illum ex dolorum ut, ipsam ipsa vel doloribus, veritatis incidunt facilis ad mollitia aperiam error perspiciatis voluptate molestiae praesentium ratione quae?</p>
            <p className="mt-5 "> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minima illum ex dolorum ut, ipsam ipsa vel doloribus, veritatis incidunt facilis ad mollitia aperiam error perspiciatis voluptate molestiae praesentium ratione quae?</p>
